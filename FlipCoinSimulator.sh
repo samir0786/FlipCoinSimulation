05d38a97281341136584bc9b9c72b8418a2e2f3a
samir0786 <samirsiescom@gmail.com>
2020-04-05 21:16:51 +0300

    ADDED: FlipCoin Doublet Combination Function

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,29 +1,74 @@
 #!/bin/bash -x
 
-#!Declaring Varibles
-declare -A singletCount
-H=0
-T=0
+function simulator(){
 
-#!function for Flip Coin
-function flipCoin() {
-read -p "Enter How Many Times You want To FlipCoin : " flips
+#!Declaring Variables
+key=""
+flips=$1
+coins=$2
 
-for((i=0;i<flips;i++))
+#!declaring Dictionary
+declare -A doubletCount
+
+for((i=1;i<=$flips;i++))
 do
-   if(($((RANDOM % 2)) == 1 ))
-   then
-       singletCount["result"$i]="Head"
-         ((H++))
-   else
-       singletCount["result"$i]="Tails"
-         ((T++))
-   fi
+	for((j=1;j<=$coins;j++))
+	do
+		if(( $((RANDOM%2==0)) ))
+		then
+ 			key+=H
+		else
+ 			key+=T
+		fi
+	done
+	doubletCount[$key]=$((${doubletCount[$key]}+1))
+	key=""
 done
-echo "Heads Percentage = $(echo "scale=2; $H * 100 / $flips" | bc )"
-echo "Tails Percentage =  $(echo "scale=2; $T * 100 / $flips" | bc )"
+
+echo "All Occurrence : " ${!doubletCount[@]}
+echo "All Occurrence : " ${doubletCount[@]}
+
+#!calling calulate Percentage Function
+calcPercentage
 
 }
+
+function calcPercentage() {
+for keys in ${!doubletCount[@]}
+do
+		doubletCount[$keys]=$((${doubletCount[$keys]} * 100 / $flips))
+done
+
+echo "All Occurrence key : " ${!doubletCount[@]}
+echo "All Occurrence Percentage: " ${doubletCount[@]}
+
+}
+
+#!calling function
+function flipCoin()
+{
+	local flips=0
+	read -p "Press y to play other key for Quite : " play
+	while [ $play == 'y' ]
+	do
+		read -p "Enter How Many Times You want To FlipCoin : " flips
+		echo -e "Enter 1 for Single coin \nEnter 2 for Two Coins "
+		read choice
+		case $choice in
+			1)
+				simulator $flips 1
+				;;
+			2)
+				simulator $flips 2
+				;;
+			*)
+				echo "Invalid Option"
+				;;
+		esac
+		read -p "Press 1 to play other key for Quite : " play
+	done
+}
+
+#!Starting Program
 flipCoin
-echo "All Occurrence : "${singletCount[@]}
 
