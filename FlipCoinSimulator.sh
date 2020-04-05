2adcfde9ee676a48e2a7424b10e85d6c7ed915fb
samir0786 <samirsiescom@gmail.com>
2020-04-05 19:08:00 +0300

    ADDED: FlipCoin Singlet Combination Function

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,12 +1,29 @@
 #!/bin/bash -x
 
+#!Declaring Varibles
+declare -A singletCount
+H=0
+T=0
+
+#!function for Flip Coin
 function flipCoin() {
-   if [ $((RANDOM%2)) -eq 1 ]
+read -p "Enter How Many Times You want To FlipCoin : " flips
+
+for((i=0;i<flips;i++))
+do
+   if(($((RANDOM % 2)) == 1 ))
    then
-      echo "heads"
+       singletCount["result"$i]="Head"
+         ((H++))
    else
-      echo "Tails"
+       singletCount["result"$i]="Tails"
+         ((T++))
    fi
+done
+echo "Heads Percentage = $(echo "scale=2; $H * 100 / $flips" | bc )"
+echo "Tails Percentage =  $(echo "scale=2; $T * 100 / $flips" | bc )"
+
 }
 flipCoin
+echo "All Occurrence : "${singletCount[@]}
 
